f43e4ecf20ea8d183a6e4606c4c8e4d93201e785
RMcGirr83 <rmcgirr83@gmail.com>
2015-03-04 12:29:17 -0500

    Initialize repo...jump start travis

---
+++ migrations/m1_initial_schema.php
@@ -0,0 +1,45 @@
+<?php
+/**
+*
+* @package Genders
+* @copyright (c) 2015 Rich McGirr (RMcGirr83)
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace phpbbmodders\genders\migrations;
+
+class m1_initial_schema extends \phpbb\db\migration\migration
+{
+	public function effectively_installed()
+	{
+		return $this->db_tools->sql_column_exists($this->table_prefix . 'users', 'user_gender');
+	}
+
+	static public function depends_on()
+	{
+		return array('\phpbb\db\migration\data\v31x\v314rc1');
+	}
+
+	public function update_schema()
+	{
+		return array(
+			'add_columns'	=> array(
+				$this->table_prefix . 'users'	=> array(
+					'user_gender'	=> array('UINT', 0),
+				),
+			),
+		);
+	}
+
+	public function revert_schema()
+	{
+		return array(
+			'drop_columns' => array(
+				$this->table_prefix . 'users'	=> array(
+					'user_flag',
+				),
+			),
+		);
+	}
+}
---
+++ migrations/m2_initial_data.php
@@ -0,0 +1,29 @@
+<?php
+/**
+*
+* @package Genders
+* @copyright (c) 2015 Rich Mcgirr (RMcGirr83)
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace phpbbmodders\genders\migrations;
+
+class m2_initial_data extends \phpbb\db\migration\migration
+{
+	public function effectively_installed()
+	{
+		return isset($this->config['genders_version']) && version_compare($this->config['genders_version'], '1.0.0', '>=');
+	}
+
+	static public function depends_on()
+	{
+		return array('\phpbbmodders\genders\migrations\m1_initial_schema');
+	}
+
+	public function update_data()
+	{
+		return array(
+			array('config.add', array('genders_version', '1.0.0')),
+	}
+}
